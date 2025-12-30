e19ea62594075b85ee778a0e2e67447278fd186e
AbhishekMukundKumbhar <abhishekkumbar1919@gmail.com>
2025-12-30 19:31:34 +0530

    Updated experience and achievements

--- src/PortfolioContainer/Resume/WorkHistory/WorkHistory.js
+++ src/PortfolioContainer/Resume/WorkHistory/WorkHistory.js
@@ -1,76 +1,46 @@
-import React from 'react';
-import ResumeSubSectionHeading from '../ResumeSubSectionHeading/ResumeSubSectionHeading';
-import './WorkHistory.css';
+import React from "react";
+import ResumeSubSectionHeading from "../ResumeSubSectionHeading/ResumeSubSectionHeading";
+import "./WorkHistory.css";
+import config from "../../../config/config.json";
 
 const WorkHistory = () => {
-    const domainDetails = [
-        {
-            domain: 'Web development',
-            description: [
-                'Developing components in front end using React.js',
-                'Creating API’s using Express.js based on UI requirements.',
-                'Performing the testing of the Node.js code by creating unit test cases using Mocha–Chai frameworks.'
-            ]
-        },
-        {
-            domain: 'Android development',
-            description: [
-                'Hands on experience in android programming using java.',
-                `Analyzing the defects [logs] for finding the root cause of the issue raised in jira tool.`,
-                'With the help of root cause analysis debugging the code to resolve the issue.'
-            ]
-        }
-    ]
-    return (
-        <div id='workhistory' className='content'>
-            <ResumeSubSectionHeading headingText = {'Experience'}/>
-            <div className='experience-content'>
-                <ul>
-                    <li className='cmp-name'>HCL Technologies</li>
-                    <li>
-                        <table className='about-experience'>
-                            <tr style={{marginTop:'5%' }}>
-                                <td style={{ fontWeight: 'bold'}}>Period</td>
-                                <td>:</td>
-                                <td>Jan 28th 2021 – till date</td>
-                            </tr>
-                            <tr>
-                                <td style={{ fontWeight: 'bold' }}>Designation</td>
-                                <td>:</td>
-                                <td>Junior Developer</td>
-                            </tr>
-                        </table>
-                    </li>
-                </ul>
-                <div className='key-result-area'>
-                    <h5>Key results area :</h5>
-                    <div >
-                        <ul>
-                            {domainDetails.map(detail => {
-                                return (
-                                    <li className='key-result-details'>
-                                        <div className='key-result-section'>
-                                            <div className='domain-title'>
-                                                <h4>{detail.domain}</h4>
-                                            </div>
-                                            <div className='domain-desc'>
-                                                <ul style={{listStyleType:'initial'}}>
-                                                    {detail.description.map(desc => {
-                                                        return (
-                                                            <li>{desc}</li>
-                                                        )
-                                                    })}
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </li>
-                                )
-                            })}
+  return (
+    <div id="workhistory" className="content">
+      <ResumeSubSectionHeading headingText={"Experience"} />
+      <div className="experience-content">
+        <div className="key-result-area">
+          <div>
+            <ul>
+              {config.experience?.map((detail) => {
+                return (
+                  <li className="key-result-details">
+                    <div className="key-result-section">
+                      <div className="role-title">
+                        <h5>{detail.role}</h5>
+                      </div>
+                      <h6 className="company-name">{detail.companyName}</h6>
+                      <div className="duration-location">
+                        <div>
+                          {detail.startDate} - {detail.endDate}
+                        </div>
+                        <div>{detail.location}</div>
+                      </div>
+                      <div className="domain-desc">
+                        <ul style={{ listStyleType: "initial" }}>
+                          {detail.keyResponsibilities.map((responsibility) => {
+                            return <li>{responsibility}</li>;
+                          })}
                         </ul>
+                      </div>
                     </div>
-                </div>
-            </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 export default WorkHistory;
